b607409f14f570dbef06c221ddd768d87718156a
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-07-17 17:56:01 -0300

    menu responsivo dinamico

--- ex033/mq005/script/main.js
+++ ex033/mq005/script/main.js
@@ -1,13 +1,14 @@
-function showMenu(){
+function responsiveDynamicMenu(){
     const burguer = document.querySelector('#burguer');
     const dynamicMenu = document.querySelector('.dynamic-menu');
     const menuHeader = document.querySelector('.MenuHeader');
 
-    let isVisible = false;
 
+    function showMenu(){
+    let isVisible = false;
     menuHeader.addEventListener('click', (e) =>{
         const el = e.target;
-        if(el.id === 'burguer'){
+        if(el.id === 'burguer'){ 
             if(isVisible){
                 dynamicMenu.style.display = 'none';
             }else{
@@ -19,4 +20,19 @@ function showMenu(){
     })
 }
 
-showMenu();
+    function monitorsResizing(){
+        window.addEventListener('resize', (e) => {
+            if(window.innerWidth >= 992){
+                dynamicMenu.style.display = 'block';
+            }else{
+                dynamicMenu.style.display = 'none';
+            }
+        });
+    }
+
+    
+    showMenu();
+    monitorsResizing();
+}
+
+responsiveDynamicMenu();
